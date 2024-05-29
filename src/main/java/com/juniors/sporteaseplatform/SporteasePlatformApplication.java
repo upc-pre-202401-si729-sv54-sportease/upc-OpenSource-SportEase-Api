e2c374c9c6098ecea16dc3c7755d7f18b90f10c2
pierotm <pierotarazona822@gmail.com>
2024-05-28 21:56:05 -0500

    chore: create default project

---
+++ src/main/java/com/juniors/sporteaseplatform/SporteasePlatformApplication.java
@@ -0,0 +1,15 @@
+package com.juniors.sporteaseplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing
+public class SporteasePlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SporteasePlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/juniors/sporteaseplatform/SporteasePlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juniors.sporteaseplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SporteasePlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
